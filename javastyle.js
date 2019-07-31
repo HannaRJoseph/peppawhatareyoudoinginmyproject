1e59dc155b971499f51e6201e380c161c2550533
unknown <you@example.com>
2019-07-31 10:11:53 -0700

    first commit

---
+++ javastyle.js
@@ -0,0 +1,13 @@
+alert("Peppa whaddya doin' in my project?")
+// alert("Dam :o")
+// alert("ba dum tss :)")
+
+function moveToPage2()
+<head>
+</head>
+
+body {
+  <img src="pepper.jfif" alt="A photo of peppa pig" width=500 height=500>
+  window.location.href = "myprofile2.html";
+}
+</body>
